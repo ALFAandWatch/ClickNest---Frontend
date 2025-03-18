ffd614579c2473065bab632a616799f112177af2
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-03-18 02:39:14 -0300

    Cambie la alerta de que no hay productos para cierta categoria a español

--- front/src/components/WarningNoProducts/WarningNoProducts.tsx
+++ front/src/components/WarningNoProducts/WarningNoProducts.tsx
@@ -2,7 +2,7 @@ const WarningNoProducts = () => {
    return (
       <>
          <p className="p-2 text-gray-400 border-gray-400 rounded-lg text-center w-full border-2">
-            No products found for this category
+            No se encontraron productos para esta categoría
          </p>
       </>
    );
